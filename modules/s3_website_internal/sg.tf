6dee57d4666cfc5edf3130db0ba5725b470caae3
edwardboucher <edbouch@yahoo.com>
2025-03-27 00:29:18 -0400

    module needed sg rules

--- modules/s3_website_internal/sg.tf
+++ modules/s3_website_internal/sg.tf
@@ -3,6 +3,23 @@ resource "aws_security_group" "s3_endpoint_sg" {
   name        = "${var.bucket_prefix}-s3-endpoint-sg-${var.environment}"
   description = "Security group for S3 VPC endpoint - managed by Terraform"
   vpc_id      = data.aws_subnet.s3_vpce_01.vpc_id
+
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.discovered.cidr_block]
+    description = "Allow incoming HTTP connections"
+  }
+
+  # ping access from anywhere
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = [data.aws_vpc.discovered.cidr_block]
+    description = "Allow all OUT"
+  }
   
   tags = merge(
     local.common_tags,

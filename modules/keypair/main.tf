8a32b5db5d360b3d449839c95e2126a9610bee21
edwardboucher <edbouch@yahoo.com>
2025-03-28 11:48:53 -0400

    internal_web_test_03

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,11 +1,11 @@
 # Generates a secure private key and encodes it as PEM
 resource "tls_private_key" "key_pair" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  algorithm = var.algorithm
+  rsa_bits  = var.rsa_bits
 }
 
 # Create the Key Pair
 resource "aws_key_pair" "key_pair" {
-  key_name   = "linux-key-pair-guac"
+  key_name   = var.key_name
   public_key = tls_private_key.key_pair.public_key_openssh
 }

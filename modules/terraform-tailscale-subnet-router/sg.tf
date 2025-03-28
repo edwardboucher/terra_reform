9034fc58e56405648d3be5c7fef77d02ac05a7bb
edwardboucher <edbouch@yahoo.com>
2025-03-28 09:47:50 -0400

    internal_web_test_00- outputs08

--- modules/terraform-tailscale-subnet-router/sg.tf
+++ modules/terraform-tailscale-subnet-router/sg.tf
@@ -4,40 +4,32 @@ data "external" "getmyip2" {
 }
 
 resource "aws_security_group" "tailscale-node-sg" {
-  name        = "linux-sg"
+  name        = "tailscale-sg"
   description = "Allow incoming traffic to the Linux EC2 Instance"
   vpc_id      = data.aws_subnet.tf_target_subnet.vpc_id
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 3478
+    to_port     = 3478
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow incoming HTTP connections"
+    description = "STUN/TURN server for NAT traversal"
   }
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    #use MYIP
-    cidr_blocks = [format("%s/%s", data.external.getmyip2.result["internet_ip"], "32")]
-    description = "Allow incoming SSH connections"
-  }
-    ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    #use MYIP
-    cidr_blocks = [data.aws_vpc.tf_target_vpc.cidr_block]
-    description = "Allow incoming SSH connections from ansible"
+
+   ingress {
+    from_port   = 41641
+    to_port     = 41641
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "tailscale's primary coordination port"
   }
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "icmp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     #use MYIP
     cidr_blocks = [format("%s/%s", data.external.getmyip2.result["internet_ip"], "32")]
-    description = "Allow incoming ICMP connections"
+    description = "Allow incoming SSH connections from current ip"
   }
 
   egress {

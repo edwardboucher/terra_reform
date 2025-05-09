1962d220a67cf340945031403dd97855f86899cd
Edward Boucher <58921328+edwardboucher@users.noreply.github.com>
2025-05-09 12:57:53 -0400

    Update variables.tf

--- modules/global_constants/variables.tf
+++ modules/global_constants/variables.tf
@@ -1,4 +1,33 @@
 variable "string_length" {
-    type = number
-    default = 8
+  type        = number
+  description = "The length of the random string to generate."
+  default     = 16 # Reasonable default
+  validation {
+    condition     = var.string_length > 0
+    error_message = "The string length must be greater than zero."
+  }
+}
+
+variable "include_special_chars" {
+  type        = bool
+  description = "Whether to include special characters in the random string."
+  default     = false
+}
+
+variable "include_upper_chars" {
+  type        = bool
+  description = "Whether to include uppercase characters in the random string."
+  default     = true
+}
+
+variable "include_lower_chars" {
+  type        = bool
+  description = "Whether to include lowercase characters in the random string."
+  default     = true
+}
+
+variable "include_numbers" {
+  type        = bool
+  description = "Whether to include numbers in the random string."
+  default     = true
 }

d131489ad6e92b2d507a8e4c9c04e4e29d7309e9
Edward Boucher <58921328+edwardboucher@users.noreply.github.com>
2025-06-16 16:08:22 -0400

    Update main.tf

--- modules/global_constants/main.tf
+++ modules/global_constants/main.tf
@@ -1,7 +1,11 @@
-resource "random_string" "random_suffix" {
-  length  = var.string_length
-  special = var.include_special_chars
-  upper   = var.include_upper_chars
-  lower   = var.include_lower_chars  # Add this to control lower case
-  numeric  = var.include_numbers # Add this to control numbers
+resource "random_string" "random" {
+  length      = var.string_length
+  special     = var.include_special_chars
+  upper       = var.include_uppercase
+  lower       = var.include_lowercase
+  number      = var.include_numbers
+  min_upper   = var.min_uppercase
+  min_lower   = var.min_lowercase
+  min_numeric = var.min_numbers
+  min_special = var.min_special
 }
